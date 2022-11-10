432fb7211bb3112bb7d2e4fd0fdc08b2710b641b
Willygodx <113140881+Willygodx@users.noreply.github.com>
2022-11-10 04:03:26 +0300

    Update task1.c

--- LAB 5/task1.c
+++ LAB 5/task1.c
@@ -3,7 +3,7 @@
 #include <mm_malloc.h>
 
 int main() {
-    int n = 99, a=100, b=100;
+    int n = 99, a = 100, b = 100;
     int* arr;
     arr = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < 50; i++)
@@ -11,19 +11,19 @@ int main() {
         arr[i] = i * 2;
         printf("%d ", arr[i]);
     }
-    while (a > b || a > 99 || b > 99 ||a<0 ||b<0 || a==b || getchar() != '\n')
+    while (a > b || a > 99 || b > 99 || a < 0 || b < 0 || a == b || getchar() != '\n')
     {
         rewind(stdin);
         printf("\nEnter line segment from a to b: ");
         scanf("%d", &a);
         scanf("%d", &b);
     }
-    if (a%2==1 && b%2==1)
+    if (a % 2 == 1 && b % 2 == 1)
         b -= 1;
 
     DeleteOddElements(arr, a, b);
     arr=(int*)realloc(arr, (b - a)*sizeof(int));
-    for (int i = 0; i < (b - a)/2+1; i++)
+    for (int i = 0; i < (b - a)/2 + 1; i++)
         printf("%d ", arr[i]);
 
     free(arr);
