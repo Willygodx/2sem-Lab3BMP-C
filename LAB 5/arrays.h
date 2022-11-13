6247c2686d2692c45a6d6802e2d3da88a8711583
Willygodx <113140881+Willygodx@users.noreply.github.com>
2022-11-14 01:38:15 +0300

    Delete LAB 5 directory

--- LAB 5/arrays.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-void OutputArray(int** arr, int n, int m);
-void OutputArrayZero(int** arr, int n, int m);
-int* DeleteOddElements(int* arr, int a, int b);
-int MinNumber(int** arr, int n);
