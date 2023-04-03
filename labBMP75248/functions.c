43adaf11c4a85e3f17ac8fd42314b6dc8260f532
willygodx <enotland34@yandex.ru>
2023-04-04 02:08:59 +0300

    1

--- labBMP75248/functions.c
+++ labBMP75248/functions.c
@@ -1,5 +1,1 @@
 #include "functions.h"
-
-
-
-
--- labBMP75248/functions.h
+++ labBMP75248/functions.h
@@ -1,3 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
+typedef uint16_t UINT;
+typedef uint32_t DWORD;
+typedef int32_t LONG;
+typedef uint16_t WORD;
+typedef uint8_t BYTE;
 
+typedef struct BITMAPFILEHEADER
+{
+    UINT bfType;
+    DWORD bfSize;
+    UINT bfReserved1;
+    UINT bfReserved2;
+    DWORD bfOffBits;
+} BITMAPFILEHEADER;
 
+typedef struct BITMAPINFOHEADER
+{
+    DWORD biSize;
+    LONG biWidth;
+    LONG biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
+} BITMAPINFOHEADER;
+
+typedef struct RGB
+{
+    BYTE Red;
+    BYTE Green;
+    BYTE Blue;
+} RGB;
