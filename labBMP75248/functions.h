6b719c27dc30ab7a376cbcee152125310513a64f
willygodx <enotland34@yandex.ru>
2023-04-12 08:22:37 +0300

    1

--- labBMP75248/functions.h
+++ labBMP75248/functions.h
@@ -1,41 +1,53 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-
+#include <math.h>
+#include <time.h>
 typedef uint16_t UINT;
 typedef uint32_t DWORD;
 typedef int32_t LONG;
-typedef uint16_t WORD;
 typedef uint8_t BYTE;
 
-typedef struct BITMAPFILEHEADER
-{
+typedef struct {
     UINT bfType;
     DWORD bfSize;
     UINT bfReserved1;
     UINT bfReserved2;
     DWORD bfOffBits;
-} BITMAPFILEHEADER;
+} __attribute__((packed)) BITMAPFILEHEADER;
 
-typedef struct BITMAPINFOHEADER
-{
+typedef struct {
     DWORD biSize;
-    LONG biWidth;
-    LONG biHeight;
-    WORD biPlanes;
-    WORD biBitCount;
+    LONG  biWidth;
+    LONG  biHeight;
+    UINT biPlanes;
+    UINT biBitCount;
     DWORD biCompression;
     DWORD biSizeImage;
-    LONG biXPelsPerMeter;
-    LONG biYPelsPerMeter;
+    LONG  biXPelsPerMeter;
+    LONG  biYPelsPerMeter;
     DWORD biClrUsed;
     DWORD biClrImportant;
-} BITMAPINFOHEADER;
+} __attribute__((packed)) BITMAPINFOHEADER;
 
-typedef struct RGB
-{
+
+typedef struct {
     BYTE red;
     BYTE green;
     BYTE blue;
-} RGB;
+} __attribute__((packed)) RGB;
+
+int checkIfBMP(const char *fileName);
+void checkValueForMedianFiltering(int* value);
+void checkValueForGammaValue(float* value);
+void checkValueForMenu(int* value);
+FILE* openFileWithChecks(const char* fileName, const char* openMode);
+FILE *copyFile(FILE *fileBMP, FILE *editedFile, const char *editedFileName);
+void convertToNegative(FILE* fileBMP, FILE* editedFile);
+void convertToBlackAndWhite(FILE* fileBMP, FILE* editedFile);
+void gammaCorrection(FILE* fileBMP, FILE* editedFile, float gammaValue);
+void medianFilter(FILE *fileBMP, FILE *editedFile, int filterSize);
+int compare(const int* first, const int* second);
+size_t sortAndMedianSize(size_t *pixelValue, const size_t length);
+void menu(FILE* fileBMP, FILE* editedFile, const char* editedFileName);
--- labBMP75248/main.c
+++ labBMP75248/main.c
@@ -1,6 +1,30 @@
 #include "functions.h"
 
-int main() {
-
+int main()
+{
+    FILE *fileBMP;
+    FILE *editedFile;
+    char *fileBMPName = NULL;
+    char *editedFileName = NULL;
+    fileBMPName = (char*) malloc(256 * sizeof(char));
+    editedFileName = (char*) malloc(4 * sizeof (char));
+    strcpy(editedFileName, "NEW");
+    printf("Enter name of the BMP file you want to edit!\n");
+    scanf("%256s", fileBMPName);
+    fileBMPName = realloc(fileBMPName, (strlen(fileBMPName) + 1) * sizeof(char));
+    editedFileName = realloc(editedFileName, (strlen(fileBMPName) + strlen(editedFileName) + 1) * sizeof(char));
+    strcat(editedFileName, fileBMPName);
+    fileBMP = openFileWithChecks(fileBMPName, "rb");
+    editedFile = openFileWithChecks(editedFileName, "wb");
+    editedFile = copyFile(fileBMP, editedFile, editedFileName);
+    menu(fileBMP, editedFile, editedFileName);
+    fclose(fileBMP);
+    fclose(editedFile);
+    free(fileBMPName);
+    free(editedFileName);
     return 0;
 }
+
+
+
+
